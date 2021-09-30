1e253eb45f5ca033086826c92516115ae0f00cdb
unknown <Pedro Fernandez>
2021-09-30 14:54:17 -0400

    Creacion del componente List

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form'
+import List from './components/List';
 import logo from './logo.svg';
 import './styles/App.css';
 
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <h1 className="title">Cree su perfil</h1>
       <div className="container">
-        <Form />
+        <List name={'Pedro Fernandez'} occupation={'Developer'}/>
       </div>
 
     </div>
---
+++ src/components/List.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/List.css'
+
+type typeProps = {
+    name: string,
+    occupation: string
+}
+
+function List({ name, occupation }: typeProps) {
+    return (
+        <>
+            <div className="box">
+                <div className="card">
+                    <div className="image">
+
+                    </div>
+                    <div className="cont">
+                        <h3>{name}</h3>
+
+                        <h5>{occupation}</h5>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+
+export default List
